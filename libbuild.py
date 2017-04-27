929609958bf7d4a12fc322cfd29380b74f6a32de
tamal <tamal@appscode.com>
2017-04-26 20:21:32 -0700

    Squashed 'hack/libbuild/' changes from ef11790..d6d6779
    d6d6779 Avoid adding .exe when uploading file.
    9306596 Use $DOCKER_REGISTRY in build command
    f2b13fe Load lib.sh from project specific files.
    cf9de3a Introduce DOCKER_REGISTRY env variable (#4)
    a512ccd Move project specific docker methods to their own files. (#3)
    f46deb7 Add k8sdb & cowrypay scripts. (#2)
    3313b96 Skip files for update_registry
    
    git-subtree-dir: hack/libbuild
    git-subtree-split: d6d67790a854e56b6f0c201052cfb5339b34d068

--- libbuild.py
+++ libbuild.py
@@ -196,12 +196,11 @@ def upload_to_cloud(folder, f, version):
     if not isinstance(buckets, dict):
         buckets = {buckets: ''}
     for bucket, region in buckets.items():
-        dst = "{bucket}/binaries/{name}/{version}/{file}{ext}".format(
+        dst = "{bucket}/binaries/{name}/{version}/{file}".format(
             bucket=bucket,
             name=name,
             version=version,
-            file=f,
-            ext='.exe' if '-windows-' in f else ''
+            file=f
         )
         if bucket.startswith('gs://'):
             upload_to_gcs(folder, f, dst, BIN_MATRIX[name].get('release', False))
@@ -237,8 +236,10 @@ def update_registry(version):
     lf = dist + '/latest.txt'
     write_file(lf, version)
     for name in os.listdir(dist):
+        if os.path.isfile(dist + '/' + name):
+            continue
         if name not in BIN_MATRIX:
-            return
+            continue
         call("gsutil cp {2} {0}/binaries/{1}/latest.txt".format(bucket, name, lf), cwd=REPO_ROOT)
         if BIN_MATRIX[name].get('release', False):
             call('gsutil acl ch -u AllUsers:R -r {0}/binaries/{1}/latest.txt'.format(bucket, name), cwd=REPO_ROOT)
