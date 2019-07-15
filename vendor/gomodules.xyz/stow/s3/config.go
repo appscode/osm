78ae69b29ed3dfb9eb98710004fa8f2914762356
Tamal Saha <tamal@appscode.com>
2019-07-15 05:50:12 -0700

    Update gomodules.xyz/stow@v0.2.0
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- vendor/gomodules.xyz/stow/s3/config.go
+++ vendor/gomodules.xyz/stow/s3/config.go
@@ -51,6 +51,10 @@ const (
 	// to establish TLS secure connection
 	ConfigCACertFile = "cacert_file"
 
+	// ConfigCACertData is optional config value for providing path to cacert data for custom endpoint like Minio
+	// to establish TLS secure connection
+	ConfigCACertData = "cacert_data"
+
 	// ConfigDisableSSL is optional config value for disabling SSL support on custom endpoints
 	// Its default value is "false", to disable SSL set it to "true".
 	ConfigDisableSSL = "disable_ssl"
@@ -173,11 +177,23 @@ func newS3Client(config stow.Config, region string) (client *s3.S3, endpoint str
 		awsConfig.WithDisableSSL(true)
 	}
 
-	cacertFile, ok := config.Config(ConfigCACertFile)
+	cacertData, ok := config.Config(ConfigCACertData)
 	if ok {
-		awsConfig.HTTPClient.Transport, err = newSecureTransport(cacertFile)
+		awsConfig.HTTPClient.Transport, err = newSecureTransport([]byte(cacertData))
 		if err != nil {
 			return nil, "", err
+		}
+	} else {
+		cacertFile, ok := config.Config(ConfigCACertFile)
+		if ok {
+			cacert, err := ioutil.ReadFile(cacertFile)
+			if err != nil {
+				return nil, "", errors.Errorf("unable to read root certificate: %v", err)
+			}
+			awsConfig.HTTPClient.Transport, err = newSecureTransport(cacert)
+			if err != nil {
+				return nil, "", err
+			}
 		}
 	}
 
@@ -199,9 +215,9 @@ func newS3Client(config stow.Config, region string) (client *s3.S3, endpoint str
 	return s3Client, endpoint, nil
 }
 
-func newSecureTransport(cacertFile string) (http.RoundTripper, error) {
-	if cacertFile == "" {
-		return nil, fmt.Errorf("invalid root certificate directory")
+func newSecureTransport(cacert []byte) (http.RoundTripper, error) {
+	if len(cacert) == 0 {
+		return nil, fmt.Errorf("missing root certificate")
 	}
 
 	tr := &http.Transport{
@@ -220,13 +236,8 @@ func newSecureTransport(cacertFile string) (http.RoundTripper, error) {
 	}
 
 	pool := x509.NewCertPool()
-
-	cacert, err := ioutil.ReadFile(cacertFile)
-	if err != nil {
-		return nil, errors.Errorf("unable to read root certificate: %v", err)
-	}
 	if ok := pool.AppendCertsFromPEM(cacert); !ok {
-		return nil, errors.Errorf("cannot parse root certificate from %q", cacertFile)
+		return nil, errors.Errorf("cannot parse root certificate from %q", string(cacert))
 	}
 	tr.TLSClientConfig.RootCAs = pool
 
